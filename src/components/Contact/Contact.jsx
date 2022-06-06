da4e063dd38c01f7bd9f27ddbd83541aed280229
cosmoinfinitas <91360858+cosmoinfinitas@users.noreply.github.com>
2022-06-06 21:31:21 +0530

    contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,32 +1,61 @@
 import React, { useContext, useRef, useState } from "react";
 import "./Contact.css";
-import emailjs from "@emailjs/browser";
 import { themeContext } from "../../Context";
 const Contact = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
   const form = useRef();
-  const [done, setDone] = useState(false)
-  const sendEmail = (e) => {
-    e.preventDefault();
+  
+  const [userData, setUserData] = useState({
+    firstName: "",
+    email: "",
+    message: "",
+  });
 
-    emailjs
-      .sendForm(
-        "service_2mu5xtl",
-        "template_m5udu2c",
-        form.current,
-        "VLwg1ltOWvnCYAiK_"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setDone(true);
-          form.reset();
-        },
-        (error) => {
-          console.log(error.text);
+  let name, value;
+  const postUserData = (event) => {
+    name = event.target.name;
+    value = event.target.value;
+
+    setUserData({ ...userData, [name]: value });
+  };
+
+  // connect with firebase
+  const submitData = async (event) => {
+    event.preventDefault();
+    const { firstName, email, message } = userData;
+
+    if (firstName && email && message) {
+      const res = fetch(
+        "https://biswaportfolio-default-rtdb.firebaseio.com/userDataRecords.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            firstName,
+            email,
+            message,
+          }),
         }
       );
+
+      if (res) {
+        setUserData({
+          firstName: "",
+
+          email: "",
+
+          message: "",
+        });
+        alert("Data Stored");
+      } else {
+        alert("plz fill the data");
+      }
+    } else {
+      alert("plz fill the data");
+    }
   };
 
   return (
@@ -35,7 +64,7 @@ const Contact = () => {
       <div className="w-left">
         <div className="awesome">
           {/* darkMode */}
-          <span style={{color: darkMode?'white': ''}}>Get in Touch</span>
+          <span style={{ color: darkMode ? "white" : "" }}>Get in Touch</span>
           <span>Contact me</span>
           <div
             className="blur s-blur1"
@@ -45,12 +74,37 @@ const Contact = () => {
       </div>
       {/* right side form */}
       <div className="c-right">
-        <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="user_name" className="user"  placeholder="Name"/>
-          <input type="email" name="user_email" className="user" placeholder="Email"/>
-          <textarea name="message" className="user" placeholder="Message"/>
-          <input type="submit" value="Send" className="button"/>
-          <span>{done && "Thanks for Contacting me"}</span>
+        <form ref={form}>
+          <input
+            type="text"
+            name="firstName"
+            className="user"
+            value={userData.firstName}
+            placeholder="Name"
+            onChange={postUserData}
+          />
+          <input
+            type="email"
+            name="email"
+            className="user"
+            value={userData.email}
+            placeholder="Email"
+            onChange={postUserData}
+          />
+          <textarea
+            name="message"
+            className="message"
+            value={userData.message}
+            placeholder="Message"
+            onChange={postUserData}
+          />
+          <input
+            type="submit"
+            value="Send"
+            className="button"
+            onClick={submitData}
+          />
+          {/* <span>{done && "Thanks for Contacting me"}</span> */}
           <div
             className="blur c-blur1"
             style={{ background: "var(--purple)" }}
